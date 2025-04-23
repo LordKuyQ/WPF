76cc15d17eb5651f91d288c810a30cf58cf0cee2
LordKuyQ <danielanhelius@gmail.com>
2025-04-24 06:50:48 +0700

    0.0.0.1

---
+++ WPF_KOZ_LAB1/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace WPF_KOZ_LAB1
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
